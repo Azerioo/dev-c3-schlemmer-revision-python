30816bd884cf1fcbb28120447c241c6e1c93d163
Azerioo <69054131+Azerioo@users.noreply.github.com>
2023-11-15 16:00:03 +0100

    Feature - Operations on list of 10 numbers - Exercise 4

---
+++ exo/ex4.py
@@ -0,0 +1,15 @@
+# Create a list of 10 numbers
+numbers = [5, 3, 7, 1, 9, 2, 8, 4, 6, 10]
+
+# Find the maximum and minimum
+maximum = max(numbers)
+minimum = min(numbers)
+
+# Calculate the average
+average = sum(numbers) / len(numbers)
+
+# Display each operation
+print("List of Numbers:", numbers)
+print("Maximum:", maximum)
+print("Minimum:", minimum)
+print("Average:", average)
