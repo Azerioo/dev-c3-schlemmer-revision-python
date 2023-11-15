d9c968fffbba944617ea81370ff892c745082b24
Azerioo <69054131+Azerioo@users.noreply.github.com>
2023-11-15 14:52:40 +0100

    Feature - Sentence input - Exercice 2

---
+++ exo/ex2.py
@@ -0,0 +1,11 @@
+# Ask user for input
+input = input("Enter a sentence: ")
+
+# Print the input in capital letters
+print("Sentence in capital letters : " + input.upper())
+
+# Print the input in lower letters
+print("Sentence in lower letters : " + input.lower())
+
+# Print the number of words in the input
+print("Words in the given sentence : ", len(input.split()))
